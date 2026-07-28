415cd6688335c13401e67d0b37d16b99bfbd10fd
mounir1 <mounir1@users.noreply.github.com>
2026-07-28 12:56:50 +0000

    deploy: d41604bf0b8a5dde46994354e03256cd6e50fa66

---
+++ assets/js/skeleton-B-t_9jrC.js
@@ -0,0 +1,1 @@
+import{j as e}from"./react-vendor-BmCUiXzP.js";import{c as s}from"./index-DbSkU7tP.js";function a({className:a,...r}){return e.jsx("div",{className:s("animate-pulse rounded-md bg-muted",a),...r})}export{a as S};
